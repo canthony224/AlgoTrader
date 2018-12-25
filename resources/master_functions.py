7d98868fef6f6fce300f1da509b2768dbf1c1c21
Khari Anthony <khari32@gmail.com>
2018-12-24 20:41:02 -0800

    Better description of function

--- resources/master_functions.py
+++ resources/master_functions.py
@@ -1,9 +1,9 @@
-# This is the first python project
 
 
-def get_currency_data():
+def get_ticker_bars(ticker, bar_granularity, bar_amount=1):
     """
-    First funk
-    :return:
+    :param ticker: the currency to get bars from
+    :param bar_granularity: the density of the bars. (5M,10M,15M,30M,1H,4H,8H,12H,1D,1W,1M)
+    :param bar_amount: number of bars to get. The default is 1 bar.
+    :return: Pandas data-frame object with Open,High,Low,Close,Volume data
     """
-    pass
